76f878bdeaf488e77c349ac8b66087f7a19bd9a1
Mimiste <mimisteus@gmail.com>
2020-09-04 17:16:08 +0200

    Added bool and vector3 primitive

---
+++ Assets/Scripts/ScriptableObjects/Primitive/BoolVariable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu]
+public class BoolVariable : ScriptableObject, ISerializationCallbackReceiver
+{
+    public bool InitialValue;
+
+    public bool RuntimeValue;
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = InitialValue;
+    }
+
+    public void OnBeforeSerialize() { }
+}
---
+++ Assets/Scripts/ScriptableObjects/Primitive/Vector3Variable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu]
+public class Vector3Variable : ScriptableObject, ISerializationCallbackReceiver
+{
+    public Vector3 InitialValue;
+
+    public Vector3 RuntimeValue;
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = InitialValue;
+    }
+
+    public void OnBeforeSerialize() { }
+}
